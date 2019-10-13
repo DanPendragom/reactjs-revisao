8b9c6afb4ce559dc306e75556d13c4dd29d7cb10
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-10-13 17:01:30 -0300

    impedindo submit vazio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 
-class App extends Component{
+class App extends Component {
   state = {
     newcontent: [
       '',
     ],
-    revisao:[
+    revisao: [
       'Styled-components',
       'Statless-components',
       'States and Properties',
@@ -16,34 +16,48 @@ class App extends Component{
   //  Toda função escrita dentro da classe, precisa estar no formato arrow function para acessar o "This"
 
   handleInputChange = e => {
-    this.setState({newcontent: e.target.value});
+    this.setState({ newcontent: e.target.value });
   }
 
-  handlesubmit = e =>{
+  handlesubmit = e => {
     // resetando função do formulario 
     e.preventDefault();
     // console.log(this.state.newcontent);
 
     // recriando array com o novo estado
-    this.setState({revisao: [...this.state.revisao, this.state.newcontent],
-      // resetando input
-      newcontent: ''
-    });
+    if(document.getElementById('input').value !== ''){
+      this.setState({
+        revisao: [...this.state.revisao, this.state.newcontent],
+        // resetando input
+        newcontent: ''
+      });
+    }
   }
-  render(){
-    return(
+
+  handledelete = e => {
+    console.log(this.state.revisao)
+  }
+
+  render() {
+    return (
       <form onSubmit={this.handlesubmit}>
         <h1>Contéudo para revisar</h1>
         <input
+          id="input"
           onChange={this.handleInputChange}
           placeholder="Adicionar novo conteúdo"
           type="text"
           value={this.state.newcontent}
         />
-        <input type="submit" value="enviar"/>
+        <input type="submit" value="enviar" />
         <ul>
           {/* Percorrendo o array revisao do estado do componente */}
-          {this.state.revisao.map(revisao => <li key={revisao}>{revisao}</li>)}
+          {this.state.revisao.map(revisao => (
+            <li key={revisao}>
+              {revisao}
+              <button onClick={this.handledelete} type="button">Remove</button>
+            </li>
+          ))}
 
           {/* exibindo novo estado no array newcontent */}
           {/* <li>{this.state.newcontent}</li> */}
