35e27cdd2a0bffb380fccc30457a028f3100c106
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-10-13 16:31:10 -0300

    memorizando valor digitado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React ,{Component} from 'react';
 
 class App extends Component{
   state = {
+    newcontent: [
+      '',
+    ],
     revisao:[
       'Styled-components',
       'Statless-components',
@@ -9,11 +12,21 @@ class App extends Component{
       'Routes'
     ]
   }
+
+  handleInputChange = e => {
+    console.log(e.target.value);
+  }
   render(){
     return(
       <div>
-        <h1>Hello World</h1>
+        <h1>Contéudo para revisar</h1>
+        <input
+          onChange={this.handleInputChange}
+          placeholder="Adicionar novo conteúdo"
+          type="text"
+        />
         <ul>
+          {/* Percorrendo o array revisao do estado do componente */}
           {this.state.revisao.map(revisao => <li key={revisao}>{revisao}</li>)}
         </ul>
       </div>
